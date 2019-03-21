b0d91e149c8e6efe54a225a3eb28cc92d5c045bb
Kananek Thongkam <info.dvgamer@gmail.com>
2019-03-21 16:29:04 +0700

    fixed ]n

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ris-line-messenger-bot
+
+```
+https://id.heroku.com/login
+USR: thkananek@central.co.th
+PWD: ASD!asd123
+```
--- bot-client/sd3-robo/index.js
+++ bot-client/sd3-robo/index.js
@@ -31,7 +31,7 @@ module.exports = {
         '`/help` ดูคำสั่งที่มีทั้งหมด',
         '`/exec` [function_name] ถ้าไม่ระบุจะเป็นเรียก func ทั้งหมดขึ้นมา',
         '`/profile` ดูข้อมูลของ chat'
-      ].join('\\n')
+      ].join('\n')
     }
   }
 }
