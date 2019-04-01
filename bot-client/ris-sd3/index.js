21e3070d94c820bcc48022644be20c4b2a814c55
Kananek Thongkam <thkananek@central.co.th>
2019-04-01 17:34:47 +0700

    add

---
+++ bot-client/ris-sd3/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  party: 'slack',
+  hooks: 'https://hooks.slack.com/services/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',
+  commands: {
+    'exec': async (args, client) => {
+    
+    },
+    'profile': async (args, client) => {
+
+    },
+    'help': async (args, client) => {
+      return [
+        '`/help` ดูคำสั่งที่มีทั้งหมด',
+        '`/exec` [function_name] ถ้าไม่ระบุจะเป็นเรียก func ทั้งหมดขึ้นมา',
+        '`/profile` ดูข้อมูลของ chat'
+      ].join('\\n')
+    }
+  }
+}
