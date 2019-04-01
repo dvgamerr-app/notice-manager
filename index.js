f3b29def00478045a627ad9bbc5fa95a4234f7a7
Kananek Thongkam <thkananek@central.co.th>
2019-04-01 17:46:06 +0700

    ris-sd3 add

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.put('/:bot/:to?', async (req, res) => {
       let { hooks } = client[bot]
       if (!hooks) throw new Error('Slack Hooks API is undefined.')
       let result = await request({ url: hooks, method: 'POST', body: req.body, json: true })
+      console.log(result)
       if (result.body !== 'ok') throw new Error(result.body)
     }
     res.json({ error: null })
