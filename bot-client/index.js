4b5d272e5d56c5cd7a88a5f61a61af025a1af62f
Kananek Thongkam <thkananek@central.co.th>
2019-04-19 10:48:05 +0700

    slack name

--- bot-client/index.js
+++ bot-client/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   'sd3-robo': require('./sd3-robo'),
-  'ris-sd3': require('./slack-sd3'),
+  'slack-sd3': require('./slack-sd3'),
   'cmgpos-bot': require('./cmgpos-bot'),
   'gamgoum': require('./gamgoum')
 }
