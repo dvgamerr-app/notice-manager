46a9d9cddd4912804859992173eb0d2cf51f9ff8
Kananek Thongkam <thkananek@central.co.th>
2019-05-10 14:17:31 +0700

    update onnline.

--- bot-client/ris-sd3/execute.js
+++ bot-client/ris-sd3/execute.js
@@ -5,11 +5,7 @@ module.exports = async (args, event) => {
   if (args[0] === 'run') {
     switch (args[1]) {
       case 'task':
-        request({
-          url: 'http://s-thcw-posdb95.pos.cmg.co.th/api/monitor/run-task/',
-          method: 'POST',
-          body: event
-        })
+        await request({ url: 'http://s-thcw-posdb95.pos.cmg.co.th/api/monitor/run-task/', method: 'POST', body: event })
         return `Copy that!, and is ordering the system.`
       default: return `Sir, Unknow job name is '${args[1]}'.`
     }
