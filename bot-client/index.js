053165712c9d2d80fa0421cfe544d563dde596ff
Kananek Thongkam <thkananek@central.co.th>
2019-05-10 15:40:52 +0700

    fixed name in line.

--- bot-client/index.js
+++ bot-client/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   'ris-sd3': require('./ris-sd3'),
+  'ris-sd4': require('./ris-sd4'),
   'slack-sd3': require('./slack-sd3'),
   'cmgpos-bot': require('./cmgpos-bot'),
   'gamgoum': require('./gamgoum')
