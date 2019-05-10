f5e312e77b302bfce7d7e30b40c826462edb58aa
Kananek Thongkam <thkananek@central.co.th>
2019-05-10 15:33:08 +0700

    comment code remove.

--- index.js
+++ index.js
@@ -88,128 +88,20 @@ app.post('/:bot', async (req, res) => {
             let result = await onPostBack[data.func].call(this, e, data, line)
             await lineMessage(e, result)
           } else {
-            // console.log(data, e)
+            console.log(data, e)
           }
         } else {
-          // console.log('e: ', e)
+          console.log('e: ', e)
         }
       }
     } else {
-      // console.log('events: ', events)
+      console.log('events: ', events)
     }
   } catch (ex) {
-    // console.log(ex.statusCode === 400 ? ex.statusMessage :  ex)
+    console.log(ex.statusCode === 400 ? ex.statusMessage :  ex)
   } finally {
     res.end()
   }
-  // U9e0a870c01ca97da20a4ec462bf72991
-//   if (events.length > 0) {
-//     const cmds = [ 'profile', 'sick', 'leave', 'watch', 'help' ]
-//     for (const event of events) {
-//       if (event.type === 'message' && event.message.type === 'text') {
-//         let { text } = event.message
-//         let cmdFound = false
-//         for (const cmd of cmds) {
-// //           let command = new RegExp(`^/${cmd}(?<arg>\\W.*|)`, 'ig')
-// //           let exec = command.exec(text)
-// //           if (!exec) continue
-
-// //           cmdFound = true
-// //           let sender = { type: 'text', text: `*CMD* ${cmd} \`${exec.groups.arg.trim()}\`` }
-
-// //           if (cmd === 'help') {
-// //             sender.text = `*Command*
-// // \`/help\` ดูคำสั่งที่มีทั้งหมด
-// // \`/sick\` [date] แจ้งขอลาป่วย \`/sick today\`
-// // \`/leave\` [date-range] ดูข้อมูลส่วนตัวของเมมเบอร์ \`/leave 2019-01-01|2019-12-31\`
-// // \`/profile\` ดูข้อมูลของ chat`
-// //             client.replyMessage(event.replyToken, sender)
-// //           } else if (cmd === 'sick') {
-// //             let { userId } = event.source
-// //             if (!exec.groups.arg.trim()) {
-// //               client.pushMessage(userId, {
-// //                 type: 'text',
-// //                 text: 'รับทราบครับ คุณต้องการลาวันที่เท่าไหร่?',
-// //                 quickReply: {
-// //                  items: [
-// //                   {
-// //                    type: 'action',
-// //                    action: { type: 'datetimepicker', label: 'Select Date', data: 'sick=1', mode: 'date', initial: '2018-09-11' }
-// //                   }
-// //                  ]
-// //                 }
-// //                })
-// //             } else {
-// //               client.getProfile(userId).then(p => {
-// //                 sender.text = `คุณ _${p.displayName}_ แจ้งขอลาป่วย *${exec.groups.arg.trim()}*`
-// //                 return client.pushMessage(groupAlert, sender)
-// //               })
-// //             }
-// //           } else if (cmd === 'leave') {
-// //             let { userId } = event.source
-// //             if (!exec.groups.arg.trim()) {
-// //               client.pushMessage(userId, {
-// //                 type: 'text',
-// //                 text: 'รับทราบครับ คุณต้องการลาวันที่เท่าไหร่?',
-// //                 quickReply: {
-// //                  items: [
-// //                   {
-// //                    type: 'action',
-// //                    action: { type: 'datetimepicker', label: 'Select Date', data: 'leave=1', mode: 'date', initial: '2018-09-11' }
-// //                   }
-// //                  ]
-// //                 }
-// //                })
-// //             } else {
-// //               client.getProfile(userId).then(p => {
-// //                 sender.text = `คุณ _${p.displayName}_ แจ้งขอลาพักร้อน *${exec.groups.arg.trim()}*`
-// //                 return client.pushMessage(groupAlert, sender)
-// //               })
-// //             }
-// //           } else if (cmd === 'watch') {
-// //           } else if (cmd === 'profile') {
-// //             let { userId, type, groupId, roomId } = event.source
-// //             if (type === 'room') {
-// //               sender.text = `*RoomId:* \`${roomId}\``
-// //               return client.replyMessage(event.replyToken, sender)
-// //             } else if (type === 'group') {
-// //               sender.text = `*GroupId:* \`${groupId}\``
-// //               return client.replyMessage(event.replyToken, sender)
-// //             } else {
-// //               client.getProfile(userId).then(p => {
-// //                 sender.text = `*${p.displayName}* - ${p.statusMessage}\n\`${p.userId}\`\n\`${p.pictureUrl}\``
-// //                 return client.replyMessage(event.replyToken, sender)
-// //               })
-// //             }
-// //           }
-// //           console.log(`${getId(event)}::${cmd}`)
-// //           break
-//         }
-//         if (!cmdFound) {
-//           let sender = { type: 'text', text: event.message.text }
-//           client.pushMessage(getId(event), sender)
-//           console.log(`${getId(event)}::${event.message.text}`)
-//         }
-//       } else if (event.type === 'postback') {
-//         // { type: 'postback',
-//         // replyToken: '3c8a1c7077664cf396db73883c1adeb2',
-//         // source: { userId: 'U9e0a870c01ca97da20a4ec462bf72991', type: 'user' },
-//         // timestamp: 1551941106824,
-//         // postback: { data: 'sick=1', params: { date: '2018-09-28' } } }
-//         console.log(event)
-//       }
-//     }
-//   } else if (events.type === 'join') {
-//     let { groupId } = events.source
-//     console.log(`${groupId}::joined`)
-    
-//   } else if (events.type === 'leave') {
-//     let { groupId } = events.source
-//     console.log(`${groupId}::leaved`)
-//   } else {
-//     console.log(events)
-//   }
-//   res.end()
 })
 
 app.get('/', (req, res) => res.end('LINE Messenger Bot Endpoint.'))
