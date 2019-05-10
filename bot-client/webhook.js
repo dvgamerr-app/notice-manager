1a2b2c0729f6fa2bfb9f56cd5d53a4db68d5df14
Kananek Thongkam <thkananek@central.co.th>
2019-05-10 12:18:22 +0700

    add await  request

--- bot-client/webhook.js
+++ bot-client/webhook.js
@@ -1,9 +1,9 @@
 const request = require('request-promise')
 const ngrok = require('./ngrok')
 
-module.exports = body => {
+module.exports = async body => {
   try {
-    request({ method: 'POST', url: ngrok, body: body, json: true })
+    await request({ method: 'POST', url: ngrok, body: body, json: true })
   } catch (ex) {
     console.warn('request webhook to ngrok -- ', ex.message)
     console.error(ex.stack)
