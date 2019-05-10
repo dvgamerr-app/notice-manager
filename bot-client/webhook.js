ae856c62b5db2b78cdc355333b7bf5b4d892a939
Kananek Thongkam <thkananek@central.co.th>
2019-05-10 12:15:55 +0700

    fixed ngrok request.

--- bot-client/webhook.js
+++ bot-client/webhook.js
@@ -1,9 +1,11 @@
 const request = require('request-promise')
+const ngrok = require('./ngrok')
 
-module.exports = (body) => {
-  return request({
-    method: 'POST',
-    url: require('./ngrok'),
-    body: body
-  })
+module.exports = body => {
+  try {
+    request({ method: 'POST', url: ngrok, body: body, json: true })
+  } catch (ex) {
+    console.warn('request webhook to ngrok -- ', ex.message)
+    console.error(ex.stack)
+  }
 }
