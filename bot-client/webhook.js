2b4eeb65a4e64d7766bcfef923bf424e6359882a
Kananek Thongkam <thkananek@central.co.th>
2019-05-10 17:29:19 +0700

    add logs.

--- bot-client/webhook.js
+++ bot-client/webhook.js
@@ -3,6 +3,8 @@ const ngrok = require('./ngrok')
 
 module.exports = async body => {
   try {
+    console.log('request -- ngrok:', ngrok)
+    console.log(body)
     await request({ method: 'POST', url: ngrok, body: body, json: true })
   } catch (ex) {
     console.warn('request webhook to ngrok -- ', ex.message)
