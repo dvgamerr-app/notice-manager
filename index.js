f281a94e62833a6865e20f10e21e85d00ed0463f
Kananek Thongkam <info.dvgamer@gmail.com>
2019-05-15 23:01:30 +0700

    update stats page.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const request = require('request-promise')
 const bodyParser = require('body-parser')
+const mongo = require('./mongodb')
 
 const port = process.env.PORT || 4000
 const app = express()
@@ -18,34 +20,78 @@ app.post('/db/:bot/cmd/:id', require('./route-db/bot-cmd'))
 app.get('/db/:bot/inbound', () => {})
 app.get('/db/:bot/outbound', () => {})
 
-
-app.get('/db/inbound', async (req, res) => {
-  // let { p } = req.params
-  res.json((await mongo.get('LineInbound').find({}, null, { sort: { created: -1 }, skip: 0, limit: 1000 })) || [])
-  res.end()
-})
-
-app.get('/db/outbound', async (req, res) => {
-  // let { p } = req.params
-  res.json((await mongo.get('LineOutbound').find({}, null, { sort: { created: -1 }, skip: 0, limit: 1000 })) || [])
-  res.end()
-})
-
-app.get('/db/cmd/:bot', async (req, res) => {
-  let { bot } = req.params
-  let opts = { limit: 100 }
-  let filter = { executed: false, botname: bot }
-
-  res.json((await mongo.get('LineCMD').find(filter, null, opts)) || [])
+app.get('/stats', async (req, res) => {
+  const { LineBot } = mongo.get()
+  try {
+      
+    let data = await LineBot.find({ type: 'line' })
+    for (const line of data) {
+      let { stats } = line.options
+      res.write(`
+        <div>
+          <b>${line.name}</b> (${line.botname})
+          <span>Monthly Usage: ${stats.usage}</span>
+          <span>Limited: ${!stats.limited ? 'none' : stats.limited}</span>
+        </div>
+      `)
+    }
+  } catch (ex) {
+    console.log(ex)
+  }
   res.end()
 })
 
 app.get('/', (req, res) => res.end('LINE Messenger Bot Endpoint.'))
+// app.get('/db/inbound', async (req, res) => {
+//   // let { p } = req.params
+//   res.json((await mongo.get('LineInbound').find({}, null, { sort: { created: -1 }, skip: 0, limit: 1000 })) || [])
+//   res.end()
+// })
+
+// app.get('/db/outbound', async (req, res) => {
+//   // let { p } = req.params
+//   res.json((await mongo.get('LineOutbound').find({}, null, { sort: { created: -1 }, skip: 0, limit: 1000 })) || [])
+//   res.end()
+// })
+
+// app.get('/db/cmd/:bot', async (req, res) => {
+//   let { bot } = req.params
+//   let opts = { limit: 100 }
+//   let filter = { executed: false, botname: bot }
+
+//   res.json((await mongo.get('LineCMD').find(filter, null, opts)) || [])
+//   res.end()
+// })
+const lineInitilize = async () => {
+  const { LineBot } = mongo.get()
+      
+  let data = await LineBot.find({ type: 'line' })
+  for (const line of data) {
+    let quota = await request('https://api.line.me/v2/bot/message/quota', {
+      headers: { 'Authorization': `Bearer ${line.accesstoken}` },
+      json: true
+    })
+    let consumption = await request('https://api.line.me/v2/bot/message/quota/consumption', {
+      headers: { 'Authorization': `Bearer ${line.accesstoken}` },
+      json: true
+    })
+    let reply = await request('https://api.line.me/v2/bot/message/delivery/reply?date=20190514', {
+      headers: { 'Authorization': `Bearer ${line.accesstoken}` },
+      json: true
+    })
+    let push = await request('https://api.line.me/v2/bot/message/delivery/push?date=20190514', {
+      headers: { 'Authorization': `Bearer ${line.accesstoken}` },
+      json: true
+    })
+    console.log(line.botname, quota, consumption, reply, push)
+  }
+}
+
 
 if (!process.env.MONGODB_URI) throw new Error('Mongo connection uri is undefined.')
 
-const mongo = require('./mongodb')
-mongo.open().then(async () => {
+lineInitilize().then(async () => {
+  await mongo.open()
   console.log(`LINE-BOT MongoDB Connected.`)
 
   await app.listen(port)
