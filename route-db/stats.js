e3d6347ccebe1707a7fa721711857f71320466be
Kananek Thongkam <info.dvgamer@gmail.com>
2019-05-29 16:07:01 +0700

    fixed schema.

--- route-db/stats.js
+++ route-db/stats.js
@@ -4,6 +4,13 @@ module.exports = async (req, res) => {
   let { LineBot } = mongo.get() // LineInbound, LineOutbound, LineCMD, 
   try {
     let data = await LineBot.find({ type: 'line' }, null, { sort: { botname: 1 } })
+    data = data.map(e => {
+      return {
+        botname: e.botname,
+        name: e.name,
+        stats: e.options.stats
+      }
+    })
     res.json(data)
     // res.write(`
     // <html lang="en">
