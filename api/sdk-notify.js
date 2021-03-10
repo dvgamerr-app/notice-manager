08ccd74be42f3c1f0312621e87c6cc06fe40fc2d
Kananek Thongkam <info.dvgamer@gmail.com>
2021-03-11 00:20:26 +0700

    remove logs.

--- api/sdk-notify.js
+++ api/sdk-notify.js
@@ -34,16 +34,12 @@ const pushNotify = async (accessToken, message) => {
     if (!message[key]) { delete message[key] }
   }
 
-  // eslint-disable-next-line no-console
-  console.log('req:', qs.stringify(message))
   const res = await instance({
     method: 'POST',
     headers: { Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/x-www-form-urlencoded' },
     url: `${apiLINE}/notify`,
     data: qs.stringify(message)
   })
-  // eslint-disable-next-line no-console
-  console.log('res:', res.data)
   return res
 }
 const getToken = async (service, room) => {
