3b848c321ecbd6956cec251aadafe13fd0d497d8
Kananek Thongkam <info.dvgamer@gmail.com>
2021-04-19 11:18:08 +0700

    fixed botname in bot api.

--- api/line-bot/push-message.js
+++ api/line-bot/push-message.js
@@ -3,41 +3,41 @@ const logger = require('@touno-io/debuger')('API')
 const sdkClient = require('../sdk-client')
 
 module.exports = async (req) => {
+  if (!req.payload || !req.payload.type) { return { OK: false } }
+
   const startTime = new Date().getTime()
-  const { bot: botname } = req.params
-  let { to: roomId } = req.params
-
-  await notice.open()
-  const { LineOutbound, LineBotRoom } = notice.get()
-
-  if (!req.payload || !req.payload.type) { throw new Error('LINE API fail formatter.') }
-  if (!/^[RUC]{1}/g.test(roomId) && (!/[0-9a-f]*/.test(roomId) || roomId.length !== 32)) {
-    const room = await LineBotRoom.findOne({ botname, name: roomId })
-    if (!room || !room.active) { throw new Error('Room name is unknow.') }
-    roomId = room.id
-  }
-
-  const { pushMessage } = await sdkClient(botname)
-  const outbound = await new LineOutbound({
-    botname,
-    userTo: roomId,
-    type: roomId.startsWith('R') ? 'room' : roomId.startsWith('C') ? 'group' : roomId.startsWith('U') ? 'user' : 'replyToken',
-    sender: req.payload || {},
-    sended: false,
-    error: null,
-    created: new Date()
-  }).save()
-
+  const { botname } = req.params
+  let { to: userTo } = req.params
+  let outbound = null
   try {
-    await pushMessage(roomId, req.payload)
-    await LineOutbound.updateOne({ _id: outbound._id }, { $set: { sended: true } })
-  } catch (ex) {
-    req.log('error', ex)
-    await notice.get('LineOutbound').updateOne({ _id: outbound._id }, { $set: { error: ex.message || ex.toString() } })
-    throw ex
-  } finally {
-    logger.info(`used ${new Date().getTime() - startTime}ms.`)
-  }
+    await notice.open()
+    const { LineOutbound, LineBotRoom } = notice.get()
 
-  return { OK: true }
+    if (!/^[RUC]{1}/g.test(userTo) && (!/[0-9a-f]*/.test(userTo) || userTo.length !== 32)) {
+      const room = await LineBotRoom.findOne({ botname, name: userTo })
+      if (!room || !room.active) { return { OK: false } }
+      userTo = room.id
+    }
+
+    const { pushMessage } = await sdkClient(botname)
+    outbound = await new LineOutbound({
+      botname,
+      userTo,
+      type: userTo.startsWith('R') ? 'room' : userTo.startsWith('C') ? 'group' : userTo.startsWith('U') ? 'user' : 'replyToken',
+      sender: req.payload || {},
+      sended: false,
+      error: null,
+      created: new Date()
+    }).save()
+
+    await pushMessage(userTo, req.payload)
+    await LineOutbound.updateOne({ _id: outbound._id }, { $set: { sended: true } })
+    return { OK: true }
+  } catch (ex) {
+    logger.error(ex)
+    if (outbound && outbound._id) { await notice.get('LineOutbound').updateOne({ _id: outbound._id }, { $set: { error: ex.message || ex.toString() } }) }
+    return { OK: false }
+  } finally {
+    logger.info(`BOT:${botname} To:${req.params.to} used ${new Date().getTime() - startTime}ms.`)
+  }
 }
