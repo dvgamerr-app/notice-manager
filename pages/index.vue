5f64993111345b3a3e5b8dbfe5f4579838715ea9
Kananek Thongkam <info.dvgamer@gmail.com>
2021-05-28 21:54:35 +0700

    add ui idex.

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,54 @@
 <template>
-  <b-container fluid />
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <b-container class="my-3 markdown-body" v-html="getReadme()" />
 </template>
 
 <script>
+import marked from 'marked'
+
 export default {
+  async asyncData () {
+  },
   data: () => ({
-    sync: {
-      bot: false
+    readme: `
+# LINE-BOT and LINE Notify Web GUI
+สร้าง http api แบบง่ายๆ โดยใช้เพียง url webhook เท่านั้น
+
+### Supported
+- LINE Notify
+- LINE BOT \`feature\`
+- Discord \`feature\`
+- Telegram \`feature\`
+
+## UI Sample
+
+| Notify | Room | Detail |
+| --- | ----------- | ----------- |
+| ![notify](/liff-notify.png) | ![room](/liff-room.png) | ![detail](/liff-detail.png) |
+
+## Rich Menu
+
+![richmenu](/richmenu.jpg)
+`
+  }),
+  methods: {
+    getReadme () {
+      return marked(this.readme)
     }
-  })
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import url("~/assets/scss/github-markdown.css");
 
+.markdown-body {
+  img {
+    max-width:100%;
+    max-height:100%;
+  }
+  code {
+    font-size: 0.85rem;
+  }
+}
 </style>
