32530ffc91ff2eaa37fb533b9ad92cf3b2408087
Kananek Thongkam <info.dvgamer@gmail.com>
2021-11-29 22:34:29 +0700

    fixed sdk line.

--- api/sdk-client.js
+++ api/sdk-client.js
@@ -1,31 +1,31 @@
 const sdk = require('@line/bot-sdk')
 const { notice } = require('@touno-io/db/schema')
 
-module.exports = async (botname) => {
+module.exports = async (botName) => {
   const { LineBot } = notice.get()
-  const client = await LineBot.findOne({ botname })
+  const client = await LineBot.findOne({ service: botName })
 
   if (!client) { throw new Error('LINE API bot is undefined.') }
-  const { accesstoken, secret } = client
+  const { accesstoken } = client
 
-  if (!accesstoken || !secret) { throw new Error('LINE Channel AccessToken is undefined.') }
+  if (!accesstoken) { throw new Error('LINE Channel AccessToken is undefined.') }
 
-  const line = new sdk.Client({ channelAccessToken: accesstoken, channelSecret: secret })
+  const line = new sdk.Client({ channelAccessToken: accesstoken })
   const convertSender = msg => typeof msg === 'string' ? { type: 'text', text: msg } : typeof msg === 'function' ? msg() : msg
   const linePushId = ({ source }) => source[`${source.type}Id`]
   const pushMessage = async (to, sender) => {
-    if (!sender) { return }
+    if (!sender) { return {} }
 
     if (typeof to === 'string') {
       if (!/^[RUC]{1}/g.test(to)) {
-        await line.replyMessage(to, convertSender(sender))
+        return await line.replyMessage(to, convertSender(sender))
       } else {
-        await line.pushMessage(to, convertSender(sender))
+        return await line.pushMessage(to, convertSender(sender))
       }
     } else if (to.replyToken) {
-      await line.replyMessage(to.replyToken, convertSender(sender))
+      return await line.replyMessage(to.replyToken, convertSender(sender))
     } else {
-      await line.pushMessage(linePushId(to), convertSender(sender))
+      return await line.pushMessage(linePushId(to), convertSender(sender))
     }
   }
 
