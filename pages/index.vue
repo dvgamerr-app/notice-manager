fd2ea2e3a9a7d2cc369a427d7d2b5ff1f6f59387
Kananek Thongkam <info.dvgamer@gmail.com>
2022-03-30 17:32:32 +0700

    fixed home

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template lang="md">
+## LINE-BOT and LINE Notify Web GUI
+สร้าง http api แบบง่ายๆ โดยใช้เพียง url webhook เท่านั้น
+
+### Supported
+- LINE Notify
+- LINE BOT `feature`
+- Discord `feature`
+- Telegram `feature`
+
+### UI Sample
+
+| Notify | Room | Detail |
+| --- | ----------- | ----------- |
+| ![notify](~/static/liff-notify.png) | ![room](~/static/liff-room.png) | ![detail](~/static/liff-detail.png) |
+
+### Rich Menu
+
+![richmenu](/richmenu.jpg)
 
 </template>
-
 <style lang="scss" scoped>
 img {
   max-width:100%;
