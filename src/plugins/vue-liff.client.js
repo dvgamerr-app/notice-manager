d4157b82ed2a7149d3b5254ab3aaaf736d604fc7
Kananek Thongkam <info.dvgamer@gmail.com>
2022-06-24 16:32:34 +0700

    update tem profile and base api.

--- src/plugins/vue-liff.client.js
+++ src/plugins/vue-liff.client.js
@@ -1,11 +1,19 @@
+/* eslint-disable no-console */
 import liff from '@line/liff'
 const { Octokit } = require('@octokit/core')
 
-export default (context, inject) => {
-  const apiNotice = new Octokit({ baseUrl: 'http://localhost:3001/api' })
+export default ({ env }, inject) => {
+  const apiNotice = new Octokit({ baseUrl: env.devEnv ? 'http://localhost:3000/api' : 'https://notice.touno.io/api' })
 
   inject('liff', liff)
   inject('api', apiNotice)
+
+  inject('tempProfile', {
+    userId: env.userId,
+    displayName: 'User',
+    statusMessage: 'Status',
+    pictureUrl: 'https://www.icmetl.org/wp-content/uploads/2020/11/user-icon-human-person-sign-vector-10206693.png'
+  })
 
   // You can access liff.init()'s return value (Promise object)
   // as this.$liffInit() by inject()
